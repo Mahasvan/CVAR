a1b5deefb1135333a82af8a7c9740f47df857ab7
Mahas1 <mahasvan@gmail.com>
2023-10-31 18:34:57 +0530

    Fix stuff

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ while True:
     if ids is not None:
         for marker_id, marker_corners in zip(ids, corners):
 
-            if marker_id[0] == 1:
-                image_to_overlay = overlay_image_1
-            elif marker_id[0] == 2:
+            if marker_id[0] == 2:
                 image_to_overlay = overlay_image_2
+            else:
+                image_to_overlay = overlay_image_1
 
             print(marker_id)
             print(list(marker_corners[0]))
@@ -47,16 +47,16 @@ while True:
             overlay_warped = cv2.warpPerspective(image_to_overlay, perspective_transform, (frame.shape[1], frame.shape[0]))
             print(overlay_resized.shape, overlay_warped.shape)
 
-            # alpha = 0.4
-            # frame = cv2.addWeighted(frame, alpha, overlay_warped, 1-alpha, 1)
-            frame_h, frame_w = frame.shape[:2]
-            overlay_mask = np.zeros((frame_h, frame_w, 3), dtype=np.uint8)
+            alpha = 0.4
+            frame = cv2.addWeighted(frame, alpha, overlay_warped, 1-alpha, 1)
+            # frame_h, frame_w = frame.shape[:2]
+            # overlay_mask = np.zeros((frame_h, frame_w, 3), dtype=np.uint8)
             
-            # Copy the overlay onto the mask using the marker's corner points
-            overlay_mask[int(marker_corners[0][0][1]):int(marker_corners[0][2][1]), int(marker_corners[0][0][0]):int(marker_corners[0][1][0])] = overlay_warped
+            # # Copy the overlay onto the mask using the marker's corner points
+            # overlay_mask[int(marker_corners[0][0][1]):int(marker_corners[0][2][1]), int(marker_corners[0][0][0]):int(marker_corners[0][1][0])] = overlay_warped
             
-            # Add the mask to the frame
-            frame = cv2.add(frame, overlay_mask)
+            # # Add the mask to the frame
+            # frame = cv2.add(frame, overlay_mask)
 
 
     cv2.imshow('ArUco Marker with Overlay', frame)
